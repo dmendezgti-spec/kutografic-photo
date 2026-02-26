35e252e5f8f6eec5dbeb0b5f1e7e2eaab78ff8ca
Daniel Méndez Godoy <d.mendezgti@gmail.com>
2026-02-26 18:27:12 +0000

    File changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import HeroSection from "@/components/sections/HeroSection";
 import MetricsSection from "@/components/sections/MetricsSection";
 import ServicesSection from "@/components/sections/ServicesSection";
 
+export const runtime = 'edge';
+
 export default function Home() {
   return (
     <div className="flex flex-col">
