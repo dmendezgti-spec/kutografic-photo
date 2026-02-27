d3503d5312b10c5bcd78d5071128cb35c8e9562c
Daniel Méndez Godoy <d.mendezgti@gmail.com>
2026-02-27 13:10:10 +0000

    10:07:08.364	Cloning repository... 10:07:09.472	From https://github.com/

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,10 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        iconLeft: ({ className, ...props }) => (
+        IconLeft: ({ className, ...props }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
         ),
-        iconRight: ({ className, ...props }) => (
+        IconRight: ({ className, ...props }) => (
           <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
